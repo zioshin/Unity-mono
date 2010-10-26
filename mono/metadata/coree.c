2b29c96b0337c769830672221bb885935f2e91d2
Lucas Meijer <lucas@lucasmeijer.com>
2010-10-26 15:19:13 +0200

    ifdef coree code based on USE_COREE, and not PLATFORM_WIN32

--- mono/metadata/coree.c
+++ mono/metadata/coree.c
@@ -9,8 +9,7 @@
 
 #include <config.h>
 
-#ifdef HOST_WIN32
-
+#if (defined (HOST_WIN32) && defined (USE_COREE))
 #if _WIN32_WINNT < 0x0501
 /* Required for ACTCTX. */
 #undef _WIN32_WINNT
@@ -33,9 +32,7 @@
 #include "environment.h"
 #include "coree.h"
 
-HMODULE coree_module_handle = NULL;
-
-static gboolean init_from_coree = FALSE;
+#ifdef PLATFORM_WIN32
 
 gchar*
 mono_get_module_file_name (HMODULE module_handle)
@@ -68,6 +65,13 @@ mono_get_module_file_name (HMODULE module_handle)
 
 	return file_name_utf8;
 }
+#endif
+
+#ifdef USE_COREE
+
+HMODULE coree_module_handle = NULL;
+
+static gboolean init_from_coree = FALSE;
 
 /* Entry point called by LdrLoadDll of ntdll.dll after _CorValidateImage. */
 BOOL STDMETHODCALLTYPE _CorDllMain(HINSTANCE hInst, DWORD dwReason, LPVOID lpReserved)
@@ -923,4 +927,4 @@ mono_fixup_exe_image (MonoImage* image)
 		MonoFixupExe ((HMODULE) image->raw_data);
 }
 
-#endif /* HOST_WIN32 */
+#endif /* HOST_WIN32 && USE_COREE */
