9fbbe1b4cda32288315dbbcc9b3e3d8fe7c1ebf7
Lucas Meijer <lucas@lucasmeijer.com>
2010-10-26 15:11:44 +0200

    Support for ignoring assembly key and exact version when loading assemblies. Fixes problem that unity profile is based of monotouch, which has a different versionnumber than regular 2.0 profile, preventing 3rd party assemblies that are built against 2.0 to work on the unity profile. Conflicts: 	mono/metadata/appdomain.h

--- mono/metadata/assembly.h
+++ mono/metadata/assembly.h
@@ -42,6 +42,7 @@ MONO_API MonoAssembly *mono_assembly_get_main   (void);
 MONO_API MonoImage    *mono_assembly_get_image  (MonoAssembly *assembly);
 MONO_API mono_bool      mono_assembly_fill_assembly_name (MonoImage *image, MonoAssemblyName *aname);
 MONO_API mono_bool      mono_assembly_names_equal (MonoAssemblyName *l, MonoAssemblyName *r);
+MONO_API mono_bool      mono_assembly_names_equal_ignore_version (MonoAssemblyName *l, MonoAssemblyName *r, mono_bool ignore_version_and_key);
 MONO_API char*         mono_stringify_assembly_name (MonoAssemblyName *aname);
 
 /* Installs a function which is called each time a new assembly is loaded. */
