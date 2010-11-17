mono/mono

Mantas' MICROLIB, DISABLE_REMOTING and DISABLE_SECURITY defines + little bits of code.  Hopefully we can get rid of this, once our monolinker.exe gets more efficient

mcs/class/corlib/System.IO/Directory.cs
@@ -40,18 +40,20 @@
 
 using System.Collections;
 using System.Security;
+#if !DISABLE_SECURITY
 using System.Security.Permissions;
+#endif
 using System.Text;
-#if NET_2_0 && !NET_2_1
+#if NET_2_0 && !NET_2_1 && !DISABLE_SECURITY
 using System.Security.AccessControl;
 #endif
-#if NET_2_0
+#if NET_2_0 && !DISABLE_SECURITY
 using System.Runtime.InteropServices;
 #endif
 
 namespace System.IO
 {
-#if NET_2_0
+#if NET_2_0 && !DISABLE_SECURITY
 	[ComVisible (true)]
 #endif
 	public
@@ -94,7 +96,7 @@ namespace System.IO
 			return CreateDirectoriesInternal (path);
 		}
 
-#if NET_2_0 && !NET_2_1
+#if NET_2_0 && !NET_2_1 && !DISABLE_SECURITY
 		[MonoTODO ("DirectorySecurity not implemented")]
 		public static DirectoryInfo CreateDirectory (string path, DirectorySecurity directorySecurity)
 		{
@@ -104,7 +106,7 @@ namespace System.IO
 
 		static DirectoryInfo CreateDirectoriesInternal (string path)
 		{
-#if !NET_2_1
+#if !NET_2_1 && !DISABLE_SECURITY
 			if (SecurityManager.SecurityEnabled) {
 				new FileIOPermission (FileIOPermissionAccess.Read | FileIOPermissionAccess.Write, path).Demand ();
 			}
@@ -255,7 +257,7 @@ namespace System.IO
 			string result = MonoIO.GetCurrentDirectory (out error);
 			if (error != MonoIOError.ERROR_SUCCESS)
 				throw MonoIO.GetException (error);
-#if !NET_2_1
+#if !NET_2_1 && !DISABLE_SECURITY
 			if ((result != null) && (result.Length > 0) && SecurityManager.SecurityEnabled) {
 				new FileIOPermission (FileIOPermissionAccess.PathDiscovery, result).Demand ();
 			}
@@ -273,7 +275,7 @@ namespace System.IO
 			return GetFileSystemEntries (path, searchPattern, FileAttributes.Directory, FileAttributes.Directory);
 		}
 		
-#if NET_2_0 && !NET_2_1
+#if NET_2_0 && !NET_2_1 || UNITY
 		public static string [] GetDirectories (string path, string searchPattern, SearchOption searchOption)
 		{
 			if (searchOption == SearchOption.TopDirectoryOnly)
@@ -306,7 +308,7 @@ namespace System.IO
 			return GetFileSystemEntries (path, searchPattern, FileAttributes.Directory, 0);
 		}
 
-#if NET_2_0 && !NET_2_1
+#if NET_2_0 && !NET_2_1 || UNITY
 		public static string[] GetFiles (string path, string searchPattern, SearchOption searchOption)
 		{
 			if (searchOption == SearchOption.TopDirectoryOnly)
@@ -401,7 +403,7 @@ namespace System.IO
 				throw MonoIO.GetException (error);
 		}
 
-#if NET_2_0 && !NET_2_1
+#if NET_2_0 && !NET_2_1 && !DISABLE_SECURITY
 		public static void SetAccessControl (string path, DirectorySecurity directorySecurity)
 		{
 			throw new NotImplementedException ();
@@ -418,7 +420,9 @@ namespace System.IO
 			SetCreationTime (path, creationTimeUtc.ToLocalTime ());
 		}
 
+#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, UnmanagedCode = true)]
+#endif
 		public static void SetCurrentDirectory (string path)
 		{
 			if (path == null)
@@ -523,7 +527,7 @@ namespace System.IO
 			return result;
 		}
 
-#if NET_2_0 && !NET_2_1
+#if NET_2_0 && !NET_2_1 && !DISABLE_SECURITY
 		[MonoNotSupported ("DirectorySecurity isn't implemented")]
 		public static DirectorySecurity GetAccessControl (string path, AccessControlSections includeSections)
 		{

mcs/class/corlib/System.Threading/ThreadPool.cs
@@ -34,7 +34,9 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Messaging;
 using System.Runtime.InteropServices;
+#if !DISABLE_SECURITY
 using System.Security.Permissions;
+#endif
 
 namespace System.Threading {
 
@@ -66,7 +68,7 @@ namespace System.Threading {
 			return true;
 		}
 #endif
-
+		
 #if !NET_2_1		
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public static extern void GetAvailableThreads (out int workerThreads, out int completionPortThreads);
@@ -79,10 +81,12 @@ namespace System.Threading {
 
 		[MonoTODO("The min number of completion port threads is not evaluated.")]
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlThread=true)]
+		#endif
 		public static extern bool SetMinThreads (int workerThreads, int completionPortThreads);
 
-#if NET_2_0
+#if NET_2_0 && !MICRO_LIB
 		[MonoTODO("The max number of threads cannot be decremented.")]
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		[SecurityPermission (SecurityAction.Demand, ControlThread=true)]
@@ -99,7 +103,7 @@ namespace System.Threading {
 			if (callBack == null)
 				throw new ArgumentNullException ("callBack");
 
-#if NET_2_1 && !MONOTOUCH
+#if NET_2_1 && !MONOTOUCH && !MICRO_LIB
 			callBack = MoonlightHandler (callBack);
 #endif
 			IAsyncResult ar = callBack.BeginInvoke (state, null, null);
@@ -170,7 +174,9 @@ namespace System.Threading {
 		}
 #endif
 
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static bool UnsafeQueueUserWorkItem (WaitCallback callBack, object state)
 		{
 			// no stack propagation here (that's why it's unsafe and requires extra security permissions)
@@ -189,7 +195,9 @@ namespace System.Threading {
 		}
 		
 		[MonoTODO("Not implemented")]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, int millisecondsTimeOutInterval,
 			bool executeOnlyOnce) 
@@ -198,7 +206,9 @@ namespace System.Threading {
 		}
 		
 		[MonoTODO("Not implemented")]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, long millisecondsTimeOutInterval,
 			bool executeOnlyOnce) 
@@ -207,7 +217,9 @@ namespace System.Threading {
 		}
 
 		[MonoTODO("Not implemented")]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, TimeSpan timeout,
 			bool executeOnlyOnce) 
@@ -217,7 +229,9 @@ namespace System.Threading {
 
 		[MonoTODO("Not implemented")]
 		[CLSCompliant (false)]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, uint millisecondsTimeOutInterval,
 			bool executeOnlyOnce) 
@@ -227,7 +241,7 @@ namespace System.Threading {
 
 #endif
 
-#if NET_2_1 && !MONOTOUCH
+#if NET_2_1 && !MONOTOUCH && !MICRO_LIB
 		static WaitCallback MoonlightHandler (WaitCallback callback)
 		{
 			return delegate (object o) {

zioshin/Unity-mono

mcs/class/corlib/System.IO/Directory.cs
@@ -41,8 +41,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security;
+#if !DISABLE_SECURITY
 using System.Security.Permissions;
+#endif
 using System.Text;
+#if !DISABLE_SECURITY
 using System.Runtime.InteropServices;
 
 #if !MOONLIGHT
@@ -51,7 +54,9 @@ using System.Security.AccessControl;
 
 namespace System.IO
 {
+#if !DISABLE_SECURITY
 	[ComVisible (true)]
+#endif
 	public static class Directory
 	{
 
@@ -83,7 +88,7 @@ namespace System.IO
 			return CreateDirectoriesInternal (path);
 		}
 
-#if !MOONLIGHT
+#if !MOONLIGHT && !NET_2_1 && !DISABLE_SECURITY
 		[MonoLimitation ("DirectorySecurity not implemented")]
 		public static DirectoryInfo CreateDirectory (string path, DirectorySecurity directorySecurity)
 		{
@@ -93,7 +98,7 @@ namespace System.IO
 
 		static DirectoryInfo CreateDirectoriesInternal (string path)
 		{
-#if !MOONLIGHT
+#if !MOONLIGHT && !DISABLE_SECURITY
 			if (SecurityManager.SecurityEnabled) {
 				new FileIOPermission (FileIOPermissionAccess.Read | FileIOPermissionAccess.Write, path).Demand ();
 			}
@@ -243,7 +248,7 @@ namespace System.IO
 			string result = MonoIO.GetCurrentDirectory (out error);
 			if (error != MonoIOError.ERROR_SUCCESS)
 				throw MonoIO.GetException (error);
-#if !MOONLIGHT
+#if !MOONLIGHT && !DISABLE_SECURITY
 			if ((result != null) && (result.Length > 0) && SecurityManager.SecurityEnabled) {
 				new FileIOPermission (FileIOPermissionAccess.PathDiscovery, result).Demand ();
 			}
@@ -261,7 +266,7 @@ namespace System.IO
 			return GetFileSystemEntries (path, searchPattern, FileAttributes.Directory, FileAttributes.Directory);
 		}
 		
-#if !MOONLIGHT
+#if !MOONLIGHT || UNITY
 		public static string [] GetDirectories (string path, string searchPattern, SearchOption searchOption)
 		{
 			if (searchOption == SearchOption.TopDirectoryOnly)
@@ -298,7 +303,7 @@ namespace System.IO
 			return GetFileSystemEntries (path, searchPattern, FileAttributes.Directory, 0);
 		}
 
-#if !MOONLIGHT
+#if !MOONLIGHT || UNITY
 		public static string[] GetFiles (string path, string searchPattern, SearchOption searchOption)
 		{
 			if (searchOption == SearchOption.TopDirectoryOnly)
@@ -390,7 +395,7 @@ namespace System.IO
 				throw MonoIO.GetException (error);
 		}
 
-#if !MOONLIGHT
+#if !MOONLIGHT && !DISABLE_SECURITY
 		public static void SetAccessControl (string path, DirectorySecurity directorySecurity)
 		{
 			throw new NotImplementedException ();
@@ -407,7 +412,9 @@ namespace System.IO
 			SetCreationTime (path, creationTimeUtc.ToLocalTime ());
 		}
 
+#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, UnmanagedCode = true)]
+#endif
 		public static void SetCurrentDirectory (string path)
 		{
 			if (path == null)
@@ -503,7 +510,7 @@ namespace System.IO
 			return result;
 		}
 
-#if NET_4_0 || MOONLIGHT
+#if (NET_4_0 || MOONLIGHT) && !DISABLE_SECURITY
 		public static string[] GetFileSystemEntries (string path, string searchPattern, SearchOption searchOption)
 		{
 			// Take the simple way home:

mcs/class/corlib/System.Threading/ThreadPool.cs
@@ -34,7 +34,9 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Messaging;
 using System.Runtime.InteropServices;
+#if !DISABLE_SECURITY
 using System.Security.Permissions;
+#endif
 
 namespace System.Threading {
 
@@ -52,7 +54,7 @@ namespace System.Threading {
 			
 			return true;
 		}
-
+		
 #if !NET_2_1		
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public static extern void GetAvailableThreads (out int workerThreads, out int completionPortThreads);
@@ -65,12 +67,16 @@ namespace System.Threading {
 
 		[MonoTODO("The min number of completion port threads is not evaluated.")]
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlThread=true)]
+		#endif
 		public static extern bool SetMinThreads (int workerThreads, int completionPortThreads);
 
+#if !MICRO_LIB
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		[SecurityPermission (SecurityAction.Demand, ControlThread=true)]
 		public static extern bool SetMaxThreads (int workerThreads, int completionPortThreads);
+#endif
 			
 		public static bool QueueUserWorkItem (WaitCallback callBack)
 		{
@@ -82,7 +88,7 @@ namespace System.Threading {
 			if (callBack == null)
 				throw new ArgumentNullException ("callBack");
 
-#if MOONLIGHT
+#if MOONLIGHT && !MICRO_LIB
 			callBack = MoonlightHandler (callBack);
 #endif
 			if (callBack.IsTransparentProxy ()) {
@@ -162,7 +168,9 @@ namespace System.Threading {
 			throw new NotImplementedException ();
 		}
 
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static bool UnsafeQueueUserWorkItem (WaitCallback callBack, object state)
 		{
 			// no stack propagation here (that's why it's unsafe and requires extra security permissions)
@@ -188,7 +196,9 @@ namespace System.Threading {
 		}
 		
 		[MonoTODO("Not implemented")]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, int millisecondsTimeOutInterval,
 			bool executeOnlyOnce) 
@@ -197,7 +207,9 @@ namespace System.Threading {
 		}
 		
 		[MonoTODO("Not implemented")]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, long millisecondsTimeOutInterval,
 			bool executeOnlyOnce) 
@@ -206,7 +218,9 @@ namespace System.Threading {
 		}
 
 		[MonoTODO("Not implemented")]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, TimeSpan timeout,
 			bool executeOnlyOnce) 
@@ -216,7 +230,9 @@ namespace System.Threading {
 
 		[MonoTODO("Not implemented")]
 		[CLSCompliant (false)]
+		#if !DISABLE_SECURITY
 		[SecurityPermission (SecurityAction.Demand, ControlEvidence=true, ControlPolicy=true)]
+		#endif
 		public static RegisteredWaitHandle UnsafeRegisterWaitForSingleObject (WaitHandle waitObject,
 			WaitOrTimerCallback callBack, object state, uint millisecondsTimeOutInterval,
 			bool executeOnlyOnce) 
@@ -226,7 +242,7 @@ namespace System.Threading {
 
 #endif
 
-#if MOONLIGHT
+#if MOONLIGHT && !MICRO_LIB
 		static WaitCallback MoonlightHandler (WaitCallback callback)
 		{
 			return delegate (object o) {