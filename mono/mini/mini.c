df793adfd0c0bf681195a9a90bc1750cd1dfa185
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2010-12-29 16:41:25 +0100

    Fix xbox target name.

--- mono/mini/mini.c
+++ mono/mini/mini.c
@@ -2332,7 +2332,7 @@ mono_get_jit_tls_key (void)
 gint32
 mono_get_jit_tls_offset (void)
 {
-#if defined (HAVE_KW_THREAD) && !(defined (HOST_WIN32) && (defined (TARGET_PS3) || defined (TARGET_XBOX)))
+#if defined (HAVE_KW_THREAD) && !(defined (HOST_WIN32) && (defined (TARGET_PS3) || defined (TARGET_XBOX360)))
 	int offset;
 	MONO_THREAD_VAR_OFFSET (mono_jit_tls, offset);
 	return offset;
@@ -2356,7 +2356,7 @@ mono_get_lmf_tls_offset (void)
 gint32
 mono_get_lmf_addr_tls_offset (void)
 {
-#if !(defined (HOST_WIN32) && (defined (TARGET_PS3) || defined (TARGET_XBOX)))
+#if !(defined (HOST_WIN32) && (defined (TARGET_PS3) || defined (TARGET_XBOX360)))
 	int offset;
 	MONO_THREAD_VAR_OFFSET(mono_lmf_addr,offset);
 	return offset;
