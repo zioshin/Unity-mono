5b7e717b0709ab48eadea62079bae747a63baa53
Scott Flynn <scott@ubuntu.(none)>
2011-09-06 15:01:35 -0400

    Added new build scripts

---
+++ build_runtime_leon.pl
@@ -0,0 +1,43 @@
+PREFIX=`pwd`/builds/leon
+
+OUTDIR=builds/embedruntimes/leon
+
+CXXFLAGS="-g -DARM_FPU_VFP=1 -D__ARM_EABI__ -mno-thumb -march=armv7-a -mfpu=vfpv3 -mtune=cortex-a9";
+CC="arm-none-linux-gnueabi-gcc"
+CXX="arm-none-linux-gnueabi-g++"
+AR="arm-none-linux-gnueabi-ar"
+LD="arm-none-linux-gnueabi-ld"
+LDFLAGS=""
+
+CONFIG_OPTS="\
+--prefix=$PREFIX \
+--cache-file=leon_cross.cache \
+--host=arm-unknown-linux-gnueabi \
+--disable-mcs-build \
+--disable-parallel-mark \
+--disable-shared-handles \
+--with-sigaltstack=no \
+--with-tls=pthread \
+--with-glib=embedded \
+--disable-nls \
+mono_cv_uscore=yes"
+
+make clean && make distclean
+rm leon_cross.cache
+
+pushd eglib
+autoreconf -i
+popd
+autoreconf -i
+
+# Run configure
+./configure $CONFIG_OPTS CFLAGS="$CXXFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" CC="$CC" CXX="$CXX" AR="$AR" LD="$LD"
+
+# Run Make
+make && echo "Build SUCCESS!" || exit 1
+
+rm -rf $OUTDIR
+
+mkdir -p $OUTDIR	
+cp -f mono/mini/.libs/libmono.a $OUTDIR
+cp -f mono/mini/.libs/libmono.so $OUTDIR
---
+++ build_runtime_meego_nk.sh
@@ -0,0 +1,40 @@
+PREFIX=`pwd`/builds/meego
+
+OUTDIR=builds/embedruntimes/meego
+
+CXXFLAGS="-g -DARM_FPU_VFP=1 -D__ARM_EABI__ -mno-thumb -march=armv7-a -mfloat-abi=hard -mfpu=vfpv3 -mtune=cortex-a8 -lsoftlibm";
+
+LDFLAGS=""
+
+CONFIG_OPTS="\
+--prefix=$PREFIX \
+--cache-file=meego_cross.cache \
+--host=arm-unknown-linux-gnueabi \
+--disable-mcs-build \
+--disable-parallel-mark \
+--disable-shared-handles \
+--with-sigaltstack=yes \
+--with-tls=pthread \
+--with-glib=system \
+--disable-nls \
+mono_cv_uscore=yes"
+
+make clean && make distclean
+rm meego_cross.cache
+
+pushd eglib
+autoreconf -i
+popd
+autoreconf -i
+
+# Run configure
+./configure $CONFIG_OPTS CFLAGS="$CXXFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
+
+# Run Make
+make && echo "Build SUCCESS!" || exit 1
+
+rm -rf $OUTDIR
+
+mkdir -p $OUTDIR	
+cp -f mono/mini/.libs/libmono.a $OUTDIR
+cp -f mono/mini/.libs/libmono.so $OUTDIR
