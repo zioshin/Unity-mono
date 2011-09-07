8fe0ef6ee0294ec45ce28fe1009b7f33c3e439f1
David Rogers <david.rogers@unity3d.com>
2011-09-07 15:22:47 -0500

    More build box fixes

--- build_runtime_meego_nk.sh
+++ build_runtime_meego_nk.sh
@@ -1,11 +1,17 @@
 PREFIX=`pwd`/builds/meego
 
+if [ -z $UNITY_THISISABUILDMACHINE ] ; then
+BUILDDIR=/$PWD
+else
+BUILDDIR=/work/`basename $PWD`
+fi
+
 OUTDIR=builds/embedruntimes/meego
 
 CXXFLAGS="-O2 -DARM_FPU_VFP=1 -D__ARM_EABI__ -mno-thumb -march=armv7-a -mfloat-abi=hard -mfpu=vfpv3 -mtune=cortex-a8 -lsoftlibm";
 CFLAGS="$CXXFLAGS"
 
-LDFLAGS="-L`pwd`/unity"
+LDFLAGS="-L$BUILDDIR/unity"
 
 CONFIG_OPTS="\
 --prefix=$PREFIX \
@@ -14,31 +20,31 @@ CONFIG_OPTS="\
 --disable-mcs-build \
 --disable-parallel-mark \
 --disable-shared-handles \
---with-sigaltstack=yes \
+--with-sigaltstack=no \
 --with-tls=pthread \
---with-glib=system \
+--with-glib=embedded \
 --disable-nls \
 mono_cv_uscore=yes"
 
-/scratchbox/login -k -d /$PWD make clean && make distclean
+/scratchbox/login -k -d $BUILDDIR make clean && make distclean
 
-/scratchbox/login -k -d /$PWD rm meego_cross.cache
+/scratchbox/login -k -d $BUILDDIR rm meego_cross.cache
 
 pushd eglib
-/scratchbox/login -k -d /$PWD autoreconf -i
+/scratchbox/login -k -d $BUILDDIR autoreconf -i
 popd
-/scratchbox/login -k -d /$PWD autoreconf -i
+/scratchbox/login -k -d $BUILDDIR autoreconf -i
 
 # Run configure
-/scratchbox/login -k -d /$PWD ./configure $CONFIG_OPTS CFLAGS="$CXXFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
+/scratchbox/login -k -d $BUILDDIR ./configure $CONFIG_OPTS CFLAGS="$CXXFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
 
 # Run Make
-/scratchbox/login -k -d /$PWD make && echo "Build SUCCESS!" || exit 1
+/scratchbox/login -k -d $BUILDDIR make && echo "Build SUCCESS!" || exit 1
 
 rm -rf $OUTDIR
 
-mkdir -p $OUTDIR	
+mkdir -p $OUTDIR
 cp -f mono/mini/.libs/libmono.so $OUTDIR
 
 # Clean up for next build
-/scratchbox/login -k -d /$PWD make clean && make distclean
+/scratchbox/login -k -d $BUILDDIR make clean && make distclean
