cc72af88afa04b6f3695f641da81e3ea46405b89
David Rogers <david.rogers@unity3d.com>
2012-01-25 14:27:27 -0600

    Switch from dynamic to static

--- build_runtime_qnx.sh
+++ build_runtime_qnx.sh
@@ -1,18 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set up QNX dev environment
 # Taken from/opt/bbndk/NativeSDK-env-1_0_7.2942.sh
 
-if [ -z $UNITY_THISISABUILDMACHINE ] ; then
-QNX_TARGET="/opt/bbndk/target-1_0_7.2942/qnx6"
-QNX_HOST="/opt/bbndk/host-0.9.3/linux/x86"
-QNX_CONFIGURATION="/opt/bbndk/install/qnx"
-MAKEFLAGS="-I$QNX_TARGET/usr/include"
-LD_LIBRARY_PATH="$QNX_HOST/usr/lib:$LD_LIBRARY_PATH"
-PATH="$QNX_HOST/usr/bin:$QNX_CONFIGURATION/bin:$QNX_HOST/usr/qde/eclipse/jre/bin:$PATH"
-fi
 
-export QNX_TARGET QNX_HOST QNX_CONFIGURATION MAKEFLAGS LD_LIBRARY_PATH PATH
 
 PREFIX=`pwd`/../builds/qnx
 
@@ -35,7 +26,7 @@ make && echo "Build SUCCESS!" || exit 1
 rm -rf ../builds
 
 mkdir -p $OUTDIR
-cp -f mono/mini/.libs/libmono.so $OUTDIR
+cp -f mono/mini/.libs/libmono.a $OUTDIR
 
 if [ -d ../builds/monodistribution ] ; then
 rm -r ../builds/monodistribution
