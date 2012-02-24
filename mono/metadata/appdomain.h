mono/mono

Support for ignoring assembly key and exact version when loading assemblies. Fixes problem that unity profile is based of monotouch, which has a different versionnumber than regular 2.0 profile, preventing 3rd party assemblies that are built against 2.0 to work on the unity profile.

mono/metadata/appdomain.h
@@ -213,6 +213,9 @@ typedef gboolean (*MonoCoreClrPlatformCB) (const char *image_name);
 void
 mono_security_set_core_clr_platform_callback (MonoCoreClrPlatformCB callback);
 
+void
+mono_set_ignore_version_and_key_when_finding_assemblies_already_loaded(gboolean value);
+
 G_END_DECLS
 #endif /* _MONO_METADATA_APPDOMAIN_H_ */
 

zioshin/Unity-mono

mono/metadata/appdomain.h
@@ -212,6 +212,9 @@ typedef mono_bool (*MonoCoreClrPlatformCB) (const char *image_name);
 void
 mono_security_set_core_clr_platform_callback (MonoCoreClrPlatformCB callback);
 
+void
+mono_set_ignore_version_and_key_when_finding_assemblies_already_loaded(mono_bool value);
+
 MONO_END_DECLS
 
 #endif /* _MONO_METADATA_APPDOMAIN_H_ */