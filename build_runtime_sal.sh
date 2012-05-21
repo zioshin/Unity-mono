f3a7ab1a4ea1691bc85221f34232f0c1908b152b
Peter Kuhn <peter.kuhn@unity3d.com>
2012-05-21 16:25:06 -0400

    [SAL, QNX] Fixes multicore garbage collector race condition crash.

--- build_runtime_sal.sh
+++ build_runtime_sal.sh
@@ -4,7 +4,7 @@ PREFIX=`pwd`/builds/sal
 
 OUTDIR=builds/embedruntimes/sal
 
-CXXFLAGS="-DARM_FPU_VFP=1 -D__ARM_EABI__ -mno-thumb -march=armv7-a -mfpu=vfpv3 -mtune=cortex-a9 -fPIC"
+CXXFLAGS="-DARM_FPU_VFP=1 -DHAVE_ARMV6 -D__ARM_EABI__ -mno-thumb -march=armv7-a -mfpu=vfpv3 -mtune=cortex-a9 -fPIC"
 CC="arm-v7a8v2r2-linux-gnueabi-gcc"
 CXX="arm-v7a8v2r2-linux-gnueabi-g++"
 AR="arm-v7a8v2r2-linux-gnueabi-ar"
