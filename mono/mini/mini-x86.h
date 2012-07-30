38cb249370386eb6414645a4aae8527235e9b6cd
Jonathan Chambers <jonathan@unity3d.com>
2012-07-30 10:03:43 -0400

    build changes for x86 android

--- mono/mini/mini-x86.h
+++ mono/mini/mini-x86.h
@@ -59,7 +59,7 @@ struct sigcontext {
 };
 #endif /* __HAIKU__ */
 
-#if defined( __linux__) || defined(__sun) || defined(__APPLE__) || defined(__NetBSD__) || \
+#if defined( __linux__) && !defined(ANDROID) || defined(__sun) || defined(__APPLE__) || defined(__NetBSD__) || \
        defined(__FreeBSD__) || defined(__OpenBSD__)
 #define MONO_ARCH_USE_SIGACTION
 #endif
