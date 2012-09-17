10e1bc7e6ded88f7fc0acd7b54894bacbfe01bbd
Jonathan Chambers <joncham@gmail.com>
2012-09-17 13:18:46 -0400

    Define USE_MMAP and USE_MUNMAP in gcconfig.h rather than in vcproj for windows.
    Conflicts:
    	msvc/libgc.vcproj

--- libgc/include/private/gcconfig.h
+++ libgc/include/private/gcconfig.h
@@ -421,12 +421,14 @@
 #   if (defined(_MSDOS) || defined(_MSC_VER)) && (_M_IX86 >= 300) \
         || defined(_WIN32) && !defined(__CYGWIN32__) && !defined(__CYGWIN__)
 #     if defined(__LP64__) || defined(_WIN64)
-#	define X86_64
+#       define X86_64
 #     else
 #       define I386
 #     endif
 #     define MSWIN32	/* or Win64 */
 #     define mach_type_known
+#     define USE_MMAP
+#     define USE_MUNMAP
 #   endif
 #   if defined(_MSC_VER) && defined(_M_IA64)
 #     define IA64
