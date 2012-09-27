mono/mono

Fix unresolved external related to VectoredException changes.

mono/mini/exceptions-amd64.c
@@ -40,7 +40,7 @@ static MonoW32ExceptionHandler segv_handler;
 
 LPTOP_LEVEL_EXCEPTION_FILTER old_win32_toplevel_exception_filter;
 gpointer win32_vectored_exception_handle;
-gboolean win32_chained_exception_needs_run;
+extern gboolean win32_chained_exception_needs_run;
 extern int (*gUnhandledExceptionHandler)(EXCEPTION_POINTERS*);
 
 #define W32_SEH_HANDLE_EX(_ex) \

mono/mini/exceptions-x86.c
@@ -44,7 +44,7 @@ static MonoW32ExceptionHandler segv_handler;
 
 LPTOP_LEVEL_EXCEPTION_FILTER old_win32_toplevel_exception_filter;
 gpointer win32_vectored_exception_handle;
-gboolean win32_chained_exception_needs_run;
+extern gboolean win32_chained_exception_needs_run;
 extern int (*gUnhandledExceptionHandler)(EXCEPTION_POINTERS*);
 
 #ifndef PROCESS_CALLBACK_FILTER_ENABLED

zioshin/Unity-mono

Conflicts:
	mono/mini/exceptions-amd64.c
	mono/mini/exceptions-x86.c

mono/mini/exceptions-amd64.c
@@ -40,7 +40,7 @@ static MonoW32ExceptionHandler segv_handler;
 
 LPTOP_LEVEL_EXCEPTION_FILTER mono_old_win32_toplevel_exception_filter;
 gpointer mono_win32_vectored_exception_handle;
-gboolean mono_win32_chained_exception_needs_run;
+extern gboolean mono_win32_chained_exception_needs_run;
 extern int (*gUnhandledExceptionHandler)(EXCEPTION_POINTERS*);
 
 #define W32_SEH_HANDLE_EX(_ex) \

mono/mini/exceptions-x86.c
@@ -44,7 +44,7 @@ static MonoW32ExceptionHandler segv_handler;
 
 LPTOP_LEVEL_EXCEPTION_FILTER mono_old_win32_toplevel_exception_filter;
 gpointer mono_win32_vectored_exception_handle;
-gboolean mono_win32_chained_exception_needs_run;
+extern gboolean mono_win32_chained_exception_needs_run;
 extern int (*gUnhandledExceptionHandler)(EXCEPTION_POINTERS*);
 
 #ifndef PROCESS_CALLBACK_FILTER_ENABLED