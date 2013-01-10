0bba072b0467fb0a8fbea8fd8fac109f55d9fcf1
Tak <levi@unity3d.com>
2013-01-10 17:55:09 +0100

    Enforce cached flag on custom attribute info.

--- mono/mini/method-to-ir.c
+++ mono/mini/method-to-ir.c
@@ -7012,7 +7012,7 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 			if (custom && mono_custom_attrs_has_attr (custom, secman->suppressunmanagedcodesecurity)) {
 				pinvoke = FALSE;
 			}
-			if (custom)
+			if (custom && !custom->cached)
 				mono_custom_attrs_free (custom);
 
 			if (pinvoke) {
@@ -7020,7 +7020,7 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 				if (custom && mono_custom_attrs_has_attr (custom, secman->suppressunmanagedcodesecurity)) {
 					pinvoke = FALSE;
 				}
-				if (custom)
+				if (custom && !custom->cached)
 					mono_custom_attrs_free (custom);
 			}
 		} else {
