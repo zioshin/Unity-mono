827f0b458a4eb9d511e0d7eaf59baa27d548901d
Tak <levi@unity3d.com>
2014-05-14 17:35:27 +0200

    Fix build.

--- mono/metadata/icall-def.h
+++ mono/metadata/icall-def.h
@@ -255,7 +255,6 @@ ICALL(ENV_10, "get_HasShutdownStarted", ves_icall_System_Environment_get_HasShut
 ICALL(ENV_11, "get_MachineName", ves_icall_System_Environment_get_MachineName)
 ICALL(ENV_13, "get_Platform", ves_icall_System_Environment_get_Platform)
 ICALL(ENV_14, "get_ProcessorCount", mono_cpu_count)
-ICALL(ENV_UNITY2, "get_SocketSecurityEnabled", mono_unity_socket_security_enabled_get)
 ICALL(ENV_15, "get_TickCount", mono_msec_ticks)
 ICALL(ENV_16, "get_UserName", ves_icall_System_Environment_get_UserName)
 ICALL(ENV_16m, "internalBroadcastSettingChange", ves_icall_System_Environment_BroadcastSettingChange)
