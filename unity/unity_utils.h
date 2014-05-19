fd7f6f5f87f0484d8e803980c72daccfd18ea59c
Lucas Meijer <lucas@unity3d.com>
2014-05-19 14:41:24 +0200

    make mono_class_is_interface actually compile

--- unity/unity_utils.h
+++ unity/unity_utils.h
@@ -19,9 +19,12 @@ void unity_mono_redirect_output( const char *fout, const char *ferr );
  */
 void unity_mono_close_output(void);
 
+void mono_unity_set_embeddinghostname(const char* name);
 extern MonoString* mono_unity_get_embeddinghostname(void);
 
 void mono_unity_write_to_unity_log(MonoString* str);
+
+mono_bool mono_unity_class_is_interface (MonoClass* klass);
 
 #ifdef WIN32
 FILE* unity_fopen( const char *name, const char *mode );
