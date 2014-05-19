mono/mono

Limit unnecessary /dev and /proc accesses on OSX.

mono/metadata/assembly.c
@@ -588,7 +588,7 @@ mono_set_rootdir (void)
 	g_free (installdir);
 	g_free (bindir);
 	g_free (name);
-#elif defined(DISABLE_MONO_AUTODETECTION)
+#elif defined(DISABLE_MONO_AUTODETECTION) || defined (PLATFORM_MACOSX)
 	fallback ();
 #else
 	char buf [4096];

zioshin/Unity-mono

Conflicts:
	build_runtime_osx.pl
	mono/io-layer/processes.c
	mono/metadata/assembly.c
	mono/utils/mono-mmap.c
	mono/utils/mono-proclib.c

mono/metadata/assembly.c
@@ -702,7 +702,7 @@ mono_set_rootdir (void)
 	g_free (bindir);
 	g_free (name);
 	g_free (resolvedname);
-#elif defined(DISABLE_MONO_AUTODETECTION)
+#elif defined(DISABLE_MONO_AUTODETECTION) || defined (HOST_MACOSX)
 	fallback ();
 #else
 	char buf [4096];