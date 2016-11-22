af5e17cef4e54d2f9ecb6a1c2996809a83f01a1b
Tautvydas Žilys <tautvydas.zilys@gmail.com>
2016-11-22 16:34:59 +0200

    Add System.TimeZoneInfo.UseRegistryForTimeZoneInformation icall to the runtime.

--- mono/metadata/icall.c
+++ mono/metadata/icall.c
@@ -8048,6 +8048,13 @@ ves_icall_Microsoft_Win32_NativeMethods_SetPriorityClass (gpointer handle, gint3
 	return SetPriorityClass (handle, priorityClass);
 }
 
+ICALL_EXPORT MonoBoolean
+ves_icall_System_TimeZoneInfo_UseRegistryForTimeZoneInformation()
+{
+	/* Should be false on Windows Store and Xbox One once Mono supports those */
+	return TRUE;
+}
+
 #ifndef DISABLE_ICALL_TABLES
 
 #define ICALL_TYPE(id,name,first)
