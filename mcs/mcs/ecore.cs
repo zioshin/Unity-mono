942ec2e2611ea054b7c95eb8e83fb74bcae41c6a
Marek Safar <marek.safar@gmail.com>
2017-02-20 17:12:52 +0100

    [mcs] Inflate constraints of compiler generated proxy base call methods. Fixes #52294

--- mcs/mcs/ecore.cs
+++ mcs/mcs/ecore.cs
@@ -3395,6 +3395,8 @@ namespace Mono.CSharp {
 				// introduce redundant storey but with `this' only but it's tricky to avoid
 				// at this stage as we don't know what expressions follow base
 				//
+				// TODO: It's needed only when the method with base call is moved to a storey
+				//
 				if (rc.CurrentAnonymousMethod != null) {
 					if (targs == null && method.IsGeneric) {
 						targs = method.TypeArguments;
