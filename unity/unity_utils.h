e6e9d292ebc4134dca4caf33d5e1ae985d534555
Michael DeRoy <mderoy@student.umass.edu>
2017-04-18 14:13:40 -0400

    remove these functions, as they are no longer needed in vm

--- unity/unity_utils.h
+++ unity/unity_utils.h
@@ -39,7 +39,4 @@ unity_mono_method_is_inflated (MonoMethod* method);
 gboolean
 unity_mono_method_is_generic (MonoMethod* method);
 
-void mono_unity_set_data_dir(const char* dir);
-char* mono_unity_get_data_dir();
-
 #endif
