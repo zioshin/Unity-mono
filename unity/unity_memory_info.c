f325114002431a40be2866f210ef225d4db85825
Lukasz Paczkowski <lukasz@unity3d.com>
2017-05-10 16:13:39 +0200

    Create empty MonoManagedMemorySnapshot in  mono_unity_capture_memory_snapshot and free it in mono_unity_free_captured_memory_snapshot.

--- unity/unity_memory_info.c
+++ unity/unity_memory_info.c
@@ -1,11 +1,19 @@
 #include "unity_memory_info.h"
+#include <mono/metadata/object.h>
+#include <mono/metadata/object-internals.h>
+#include <mono/metadata/metadata.h>
+#include <mono/metadata/tabledefs.h>
+#include <mono/metadata/class-internals.h>
+#include <mono/metadata/domain-internals.h>
 
 MonoManagedMemorySnapshot* mono_unity_capture_memory_snapshot()
 {
-	return 0;
+	MonoManagedMemorySnapshot* snapshot;
+	snapshot = g_new0(MonoManagedMemorySnapshot, 1);
+	return snapshot;
 }
 
 void mono_unity_free_captured_memory_snapshot(MonoManagedMemorySnapshot* snapshot)
 {
-
+	g_free(snapshot);
 }
