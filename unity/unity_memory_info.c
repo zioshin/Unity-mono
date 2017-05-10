c9e189123e5dee1212b028ae45249e4b81110f18
Lukasz Paczkowski <lukasz@unity3d.com>
2017-05-10 14:54:00 +0200

    Add unity_memory_info.c/h

---
+++ unity/unity_memory_info.c
@@ -0,0 +1,11 @@
+#include "unity_memory_info.h"
+
+MonoManagedMemorySnapshot* mono_unity_capture_memory_snapshot()
+{
+	return 0;
+}
+
+void mono_unity_free_captured_memory_snapshot(MonoManagedMemorySnapshot* snapshot)
+{
+
+}
