0d9e941e3dbdeafb0e13c72152046e73b6c30281
Mike Voorhees <michaelv@unity3d.com>
2017-06-16 07:06:44 -0400

    Remove unnecessary usings from WebLevel.cs

--- mcs/class/referencesource/System.Web/Configuration/WebLevel.cs
+++ mcs/class/referencesource/System.Web/Configuration/WebLevel.cs
@@ -5,19 +5,6 @@
 //------------------------------------------------------------------------------
 
 namespace System.Web.Configuration {
-    using System.Collections;
-    using System.Configuration.Internal;
-    using System.Configuration;
-    using System.Globalization;
-    using System.IO;
-    using System.Reflection;
-    using System.Security;
-    using System.Web;
-    using System.Web.Compilation;
-    using System.Web.Configuration.Internal;
-    using System.Web.Hosting;
-    using System.Web.Util;
-    using System.Xml;
 
     // If a path is null, we need this to tell the difference between machine.config
     // or root web.config.
