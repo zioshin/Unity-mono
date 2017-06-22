e428b1dd659f549d3aaf0309692aca10bc55e0ba
Doug Piconi <dougp@unity3d.com>
2017-06-22 11:15:06 -0400

    build changes to allow a unity version of win32file

---
+++ mono/metadata/w32file-unity.c
@@ -0,0 +1,11 @@
+#include <config.h>
+#include <glib.h>
+
+ #include "File-c-api.h"
+
+gboolean
+mono_w32file_read (gpointer handle, gpointer buffer, guint32 numbytes, guint32 *bytesread)
+{
+    int error = 0;
+	return error;
+}
