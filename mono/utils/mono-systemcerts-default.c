062afcfe0ac133a722d635133a1fc2e570675db3
Jonathan Chambers <joncham@gmail.com>
2017-09-16 12:31:45 -0400

    Fix compiler errors

--- mono/utils/mono-systemcerts-default.c
+++ mono/utils/mono-systemcerts-default.c
@@ -1,9 +1,10 @@
 #include <config.h>
+#include <stddef.h>
 #include "mono-systemcerts.h"
 
 int EnumSystemCertificates(void* certStore, void** iter, int *format, int* size, void** data)
 {
-	return FALSE;
+	return 0;
 }
 
 void* OpenSystemRootStore()
