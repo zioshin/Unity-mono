c7d089c145ac747cd3ffb3c0e00937cd57123ee5
Michael DeRoy <michaelde@unity3d.com>
2018-06-21 12:35:06 -0400

    Add icall to return platform id

--- mono/metadata/icall-internals.h
+++ mono/metadata/icall-internals.h
@@ -14,6 +14,10 @@
 guint32
 ves_icall_System_CurrentSystemTimeZone_GetTimeZoneData (guint32 year, MonoArray **data, MonoArray **names, MonoBoolean *daylight_inverted);
 
+// UNITY
+MonoStringHandle
+GetUnityPlatformID(MonoError *error);
+
 // On Windows platform implementation of bellow methods are hosted in separate source file
 // icall-windows.c or icall-windows-*.c. On other platforms the implementation is still keept
 // in icall.c still declared as static and in some places even inlined.
