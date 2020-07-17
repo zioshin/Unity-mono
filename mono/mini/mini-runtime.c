mono/mono

add editorconfig back, enable pmip via debug flag (#766)

mono/mini/mini-runtime.c
@@ -3440,6 +3440,8 @@ mini_parse_debug_option (const char *option)
 		debug_options.gdb = TRUE;
 	else if (!strcmp (option, "lldb"))
 		debug_options.lldb = TRUE;
+	else if (!strcmp (option, "unity-mixed-callstack"))
+		debug_options.unity_mixed_callstack = TRUE;
 	else if (!strcmp (option, "explicit-null-checks"))
 		debug_options.explicit_null_checks = TRUE;
 	else if (!strcmp (option, "gen-seq-points"))
@@ -3909,7 +3911,7 @@ mini_init (const char *filename, const char *runtime_version)
 		mono_lldb_init ("");
 		mono_dont_free_domains = TRUE;
 	}
-	if (g_hasenv ("UNITY_MIXED_CALLSTACK")) {
+	if (mini_get_debug_options()->unity_mixed_callstack || g_hasenv ("UNITY_MIXED_CALLSTACK")) {
 		mixed_callstack_plugin_init ("");
 	}
 

zioshin/Unity-mono

mono/mini/mini-runtime.c
@@ -3923,6 +3923,8 @@ mini_parse_debug_option (const char *option)
 		mini_debug_options.llvm_disable_inlining = TRUE;
 	else if (!strcmp (option, "llvm-disable-implicit-null-checks"))
 		mini_debug_options.llvm_disable_implicit_null_checks = TRUE;
+	else if (!strcmp (option, "unity-mixed-callstack"))
+		mini_debug_options.unity_mixed_callstack = TRUE;
 	else if (!strcmp (option, "explicit-null-checks"))
 		mini_debug_options.explicit_null_checks = TRUE;
 	else if (!strcmp (option, "gen-seq-points"))
@@ -4465,6 +4467,10 @@ mini_init (const char *filename, const char *runtime_version)
 	if (mini_debug_options.lldb || g_hasenv ("MONO_LLDB")) {
 		mono_lldb_init ("");
 		mono_dont_free_domains = TRUE;
+	}
+
+	if (mini_get_debug_options()->unity_mixed_callstack || g_hasenv ("UNITY_MIXED_CALLSTACK")) {
+		mixed_callstack_plugin_init ("");
 	}
 
 #ifdef XDEBUG_ENABLED