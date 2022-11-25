e4681ae05efaf5fed09b4c5a2e5e9769e12a438b
Richard Fine <richardf@unity3d.com>
2022-11-25 13:34:34 -0500

    Run Bee in Windows jobs as well

--- .yamato/scripts/build_win_x86.bat
+++ .yamato/scripts/build_win_x86.bat
@@ -1,6 +1,10 @@
 @echo off
 git submodule update --init --recursive
 
+cd external/buildscripts
+bee
+cd ../..
+
 perl external/buildscripts/build_runtime_win.pl --stevedorebuilddeps=1
 if NOT %errorlevel% == 0 (
  echo "mono build script failed"
