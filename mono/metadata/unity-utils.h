mono/mono

Provide way for Network.OperationalStatus to be retrieved on an Android Unity runtime.
Unity does not build with MONODROID and therefore does not have access to the Xamarin.Android implementation since we're technically running the linux BCL on Android instead of the Android BCL that regular mono uses.

mono/metadata/unity-utils.h
@@ -257,4 +257,14 @@ MONO_API gboolean mono_unity_class_is_open_constructed_type (MonoClass *klass);
 
 MONO_API gboolean mono_unity_class_has_failure (const MonoClass* klass);
 
+#ifdef ANDROID
+typedef mono_bool (*android_network_up_state)(const char* ifName, uint8_t* is_up);
+
+MONO_API void
+mono_unity_set_android_network_up_state_func(android_network_up_state func);
+
+MonoBoolean
+ves_icall_Unity_Android_Network_Interface_Up_State (MonoString *ifName, MonoBoolean* is_up);
+#endif
+
 #endif

zioshin/Unity-mono

mono/metadata/unity-utils.h
@@ -257,4 +257,14 @@ MONO_API gboolean mono_unity_class_is_open_constructed_type (MonoClass *klass);
 
 MONO_API gboolean mono_unity_class_has_failure (const MonoClass* klass);
 
+#ifdef ANDROID
+typedef uint8_t (*android_network_up_state)(const char* ifName, uint8_t* is_up);
+
+MONO_API void
+mono_unity_set_android_network_up_state_func(android_network_up_state func);
+
+MonoBoolean
+ves_icall_Unity_Android_Network_Interface_Up_State (MonoString *ifName, MonoBoolean* is_up);
+#endif
+
 #endif